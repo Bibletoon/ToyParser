fa727c1a378d499d1e64764119461310494924c2
bibletoon <alex8.ivanov@gmail.com>
2022-10-12 15:39:37 +0300

    Refactor: category parser

--- ToyParser/Parsers/CategoryParser.cs
+++ ToyParser/Parsers/CategoryParser.cs
@@ -18,18 +18,8 @@ public class CategoryParser
     {
         var document = await _loader.LoadPage($"{categoryUrl}?count=45", region?.Cookie);
 
-        var pagesCountString =
-            document.QuerySelector(".pagination > .page-item:nth-last-child(2) > a")?
-                .TextContent
-                .Trim() ?? "1";
-        var pagesCount = int.Parse(pagesCountString);
-        var productsLinks = new List<string>();
-
-        for (var i = 1; i <= pagesCount; i++)
-        {
-            document = await _loader.LoadPage($"{categoryUrl}?count=45&PAGEN_5={i}", region?.Cookie);
-            productsLinks.AddRange(ParseProductLinks(document));
-        }
+        var pagesCount = ParsePagesCount(document);
+        var productsLinks = await GetAllProductsLinks(categoryUrl, region, pagesCount);
 
         var products = new ConcurrentBag<Product>();
         await Parallel.ForEachAsync(productsLinks, async (productUrl, token) =>
@@ -42,10 +32,33 @@ public class CategoryParser
         return products.ToList();
     }
 
+    private async Task<List<string>> GetAllProductsLinks(string categoryUrl, Region? region, int pagesCount)
+    {
+        var productsLinks = new List<string>();
+
+        for (var i = 1; i <= pagesCount; i++)
+        {
+            var document = await _loader.LoadPage($"{categoryUrl}?count=45&PAGEN_5={i}", region?.Cookie);
+            productsLinks.AddRange(ParseProductLinks(document));
+        }
+
+        return productsLinks;
+    }
+
     private List<string> ParseProductLinks(IDocument document)
     {
         return document.QuerySelectorAll(".product-card > .row > div:first-child > a")
             .Select(x => x.GetAttribute("href"))
             .ToList();
+    }
+
+    private int ParsePagesCount(IDocument document)
+    {
+        var pagesCountString =
+            document.QuerySelector(".pagination > .page-item:nth-last-child(2) > a")?
+                .TextContent
+                .Trim() ?? "1";
+        var pagesCount = int.Parse(pagesCountString);
+        return pagesCount;
     }
 }
