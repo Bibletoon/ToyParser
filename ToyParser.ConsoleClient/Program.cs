67b58b28def9dc0c387aa4f5f2f7a52403bdc3b1
bibletoon <alex8.ivanov@gmail.com>
2022-10-12 15:33:05 +0300

    Refactor: startup

--- ToyParser.ConsoleClient/Program.cs
+++ ToyParser.ConsoleClient/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Net;
-using System.Text;
-using AngleSharp;
+﻿using AngleSharp;
 using ToyParser;
 using ToyParser.Models;
 using ToyParser.Tools.CsvExporter;
@@ -9,7 +7,9 @@ var configuration = Configuration.Default.WithLocaleBasedEncoding();
 var loader = new PageLoader(configuration);
 var categoryParser = new CategoryParser(loader);
 
-var products = await categoryParser.Parse("https://www.toy.ru/catalog/boy_transport/", Region.RostovOnDon);
+var products = await categoryParser.Parse("https://www.toy.ru/catalog/boy_transport/", Region.SaintPetersburg);
+products.AddRange(await categoryParser.Parse("https://www.toy.ru/catalog/boy_transport/", Region.RostovOnDon));
+
 var productConverter = new ProductCvsConverter();
 var exporter = new CsvExporter<Product>(productConverter);
 
